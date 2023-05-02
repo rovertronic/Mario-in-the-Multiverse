8db6e0d69c3d0f3f97169a4f31aef3658258e00b
root <root@DESKTOP-URO3KBA.localdomain>
2023-05-02 02:40:29 -0500

    framebuffer stuff

--- include/macros.h
+++ include/macros.h
@@ -139,4 +139,20 @@
 
 #define FORCE_CRASH { *(vs8*)0 = 0; }
 
+/* fixed-point conversion macros */
+#define qu08(n)                       ((qu08_t)((n)*0x100))
+#define qu016(n)                      ((qu016_t)((n)*0x10000))
+#define qs48(n)                       ((qs48_t)((n)*0x0100))
+#define qs510(n)                      ((qs510_t)((n)*0x0400))
+#define qu510(n)                      ((qu510_t)((n)*0x0400))
+#define qs102(n)                      ((qs102_t)((n)*0x0004))
+#define qu102(n)                      ((qu102_t)((n)*0x0004))
+#define qs105(n)                      ((qs105_t)((n)*0x0020))
+#define qu105(n)                      ((qu105_t)((n)*0x0020))
+#define qs132(n)                      ((qs132_t)((n)*0x0004))
+#define qs142(n)                      ((qs142_t)((n)*0x0004))
+#define qs1516(n)                     ((qs1516_t)((n)*0x00010000))
+#define qs1616(n)                     ((qs1616_t)((n)*0x00010000))
+#define qs205(n)                      ((qs205_t)((n)*0x00000020))
+
 #endif // MACROS_H

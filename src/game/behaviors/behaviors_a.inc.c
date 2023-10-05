c281a60d91e7fd22ea406bda3bf5e2b83d4a0bb5
JoshTheBosh <JoshTheBosh06@github.com>
2023-10-05 18:00:33 -0400

    Jelly scale fix

--- src/game/behaviors/behaviors_a.inc.c
+++ src/game/behaviors/behaviors_a.inc.c
@@ -5,7 +5,7 @@
 // Jelly
 
 static struct ObjectHitbox sJellyHitbox = {
-    /* interactType:      */ INTERACT_BOUNCE_TOP,
+    /* interactType:      */ INTERACT_DAMAGE,
     /* downOffset:        */ 0,
     /* damageOrCoinValue: */ 1,
     /* health:            */ 1,
@@ -17,10 +17,15 @@ static struct ObjectHitbox sJellyHitbox = {
 };
 
 void jelly_init(void) {
+    o->oGravity = 0.0f;
+    o->oFriction = 0.999f;
+    
     obj_set_hitbox(o, &sJellyHitbox);
 }
 
 void jelly_loop(void) {
+    object_step();
+    
     switch (o->oAction) {
         case 0:
             o->oForwardVel = 0;
@@ -47,8 +52,14 @@ void jelly_loop(void) {
                 o->oAction = 0;
             }
 
-            if (cur_obj_was_attacked_or_ground_pounded()) {
-                o->oAction = 2;
+            if (using_ability(ABILITY_BUBBLE_HAT))
+            {
+                if (cur_obj_was_attacked_or_ground_pounded())
+                {
+                    o->oAction = 2;
+                } else {
+                    o->oAction = 1;
+                }
             }
             break;
         case 2:
@@ -68,7 +79,7 @@ void jelly_loop(void) {
                          1.0f + ((0.4f - (0.4f * ((f32)o->oTimer) / 20.0f)) * coss(o->oTimer * 0x1000 + 0x4000)),
                          1.0f + ((0.25f - (0.25f * ((f32)o->oTimer) / 20.0f)) * sins(o->oTimer * 0x1000)));
     }
-    if (o->oTimer >= 30) {
+    if (o->oTimer >= 22.5f) {
         obj_scale_xyz(o, 1.0f + ((0.2f - (0.2f * ((f32)o->oTimer) / 30.0f)) * sins(o->oTimer * 0x1000)),
                          1.0f + ((0.3f - (0.3f * ((f32)o->oTimer) / 30.0f)) * coss(o->oTimer * 0x1000 + 0x4000)),
                          1.0f + ((0.25f - (0.25f * ((f32)o->oTimer) / 30.0f)) * sins(o->oTimer * 0x1000)));
@@ -81,6 +92,8 @@ void jelly_loop(void) {
     o->oVelZ = o->oForwardVel * coss(o->oMoveAngleYaw);
     o->oPosX += o->oVelX;
     o->oPosZ += o->oVelZ;
+
+    o->oAngleToMario += o->oVelY; 
 }
 
 // Jellyfish Fields Platform
@@ -92,29 +105,9 @@ void jfplatform_loop(void) {
 
 // Taxi stop
 
-void taxistop_loop(void) {
-    /*s16 eventTimer = 62;
+void taxistop_loop(void)
+{
 
-    if (o->oAction >= 1) {
-        gCamera->cutscene = 1;
-    } else {
-        gCamera->cutscene = 0;
-    }
-    
-    if (gMarioObject->platform) {
-        gLakituState.curPos[0] = -1021;
-        gLakituState.curPos[1] = 9;
-        gLakituState.curPos[2] = -8046;
-        set_mario_animation(gMarioState, MARIO_ANIM_TRIPLE_JUMP_FLY);
-        approach_f32_asymptotic_bool(&gMarioState->pos[0], o->oPosX, 0.0f);
-        approach_f32_asymptotic_bool(&gMarioState->pos[2], o->oPosZ - (o->oTimer * 2), 0.0f);
-        play_transition(WARP_TRANSITION_FADE_INTO_CIRCLE, eventTimer, 255, 255, 255);
-        set_mario_action(gMarioState, TAXI_STOP_CUTSCENE, 0);
-        o->oAction = 1;
-        if (o->oTimer == 60) {
-            initiate_warp(LEVEL_G, 1, 0x0A, 0);
-        }
-    }*/
 }
 
 // Boat for Taxi Stop
@@ -144,31 +137,12 @@ void tiki_box_init(void) {
     o->oOpacity = 150;
 }
 
-void tiki_box_loop(void) {
-    f32 floorY;
-    s32 speed;
-    s16 yaw = o->oMoveAngleYaw;
-    s16 pitch = o->oMoveAnglePitch;
+void tiki_box_loop(void)
+{
+    object_step();
     
-    obj_set_hitbox(o, &sTikiHitbox);
-
-    floorY = find_floor_height(o->oPosX, o->oPosY, o->oPosZ);
-
-    if (using_ability(ABILITY_BUBBLE_HAT)) {
-        if (cur_obj_was_attacked_or_ground_pounded()) {
-            obj_mark_for_deletion(o);
-            cur_obj_disable_rendering();
-            cur_obj_become_intangible();
-            spawn_mist_particles();
-            create_sound_spawner(SOUND_GENERAL_BREAK_BOX);
-        }
-    }
-
-    o->oVelX = sins(yaw) * (f32) speed;
-    o->oVelY = sins(pitch) * (f32) speed;
-    o->oVelZ = coss(yaw) * (f32) speed;
-
-    switch (o->oBehParams2ndByte) {
+    switch (o->oBehParams2ndByte)
+    {
         case 0:
             obj_set_model(o, MODEL_TIKI_WOOD);
             break;
@@ -178,5 +152,15 @@ void tiki_box_loop(void) {
         case 2:
             obj_set_model(o, MODEL_TIKI_FLOAT);
             break;
+    }
+}
+
+// King Jellyfish
+
+void king_jellyfish_loop(void)
+{
+    switch (o->oAction)
+    {
+        
     }
 }

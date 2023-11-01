78af3e4f1a3eb7cfe3ca1c63399861b69cd00f09
MelroyOldenburger <15652953+MelroyOldenburger@users.noreply.github.com>
2023-11-01 10:43:25 +0100

    Temp push

--- levels/m/geo.inc.c
+++ levels/m/geo.inc.c
@@ -1,2 +1,3 @@
 #include "levels/m/area_1/geo.inc.c"
+#include "levels/m/area_2/geo.inc.c"
 #include "levels/m/area_4/geo.inc.c"
--- levels/m/texscroll.inc.c
+++ levels/m/texscroll.inc.c
@@ -1,127 +1,173 @@
-void scroll_m_dl_A4_Maze_A_mesh_layer_4_vtx_1() {
+void scroll_m_dl_hub_sign_mesh_layer_4_vtx_0() {
 	int i = 0;
-	int count = 32;
-	int width = 32 * 0x20;
+	int count = 4;
+	int height = 32 * 0x20;
 
-	static int currentX = 0;
-	int deltaX;
-	Vtx *vertices = segmented_to_virtual(m_dl_A4_Maze_A_mesh_layer_4_vtx_1);
+	static int currentY = 0;
+	int deltaY;
+	static int timeY;
+	float amplitudeY = 0.699999988079071;
+	float frequencyY = 0.699999988079071;
+	float offsetY = 0.05000000074505806;
+	Vtx *vertices = segmented_to_virtual(m_dl_hub_sign_mesh_layer_4_vtx_0);
 
-	deltaX = (int)(0.5 * 0x20) % width;
+	deltaY = (int)(amplitudeY * frequencyY * coss((frequencyY * timeY + offsetY) * (1024 * 16 - 1) / 6.28318530718) * 0x20);
 
-	if (absi(currentX) > width) {
-		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
 	}
 
 	for (i = 0; i < count; i++) {
-		vertices[i].n.tc[0] += deltaX;
+		vertices[i].n.tc[1] += deltaY;
 	}
-	currentX += deltaX;
+	currentY += deltaY;	timeY += 1;
+}
+
+void scroll_m_dl_A4_Maze_A_mesh_layer_4_vtx_1() {
+	int i = 0;
+	int count = 64;
+	int height = 32 * 0x20;
+
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(m_dl_A4_Maze_A_mesh_layer_4_vtx_1);
+
+	deltaY = (int)(-2.0 * 0x20) % height;
+
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentY += deltaY;
 }
 
 void scroll_m_dl_A4_Maze_B_mesh_layer_4_vtx_1() {
 	int i = 0;
-	int count = 32;
-	int width = 32 * 0x20;
+	int count = 64;
+	int height = 32 * 0x20;
 
-	static int currentX = 0;
-	int deltaX;
+	static int currentY = 0;
+	int deltaY;
 	Vtx *vertices = segmented_to_virtual(m_dl_A4_Maze_B_mesh_layer_4_vtx_1);
 
-	deltaX = (int)(0.5 * 0x20) % width;
+	deltaY = (int)(-2.0 * 0x20) % height;
 
-	if (absi(currentX) > width) {
-		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
 	}
 
 	for (i = 0; i < count; i++) {
-		vertices[i].n.tc[0] += deltaX;
+		vertices[i].n.tc[1] += deltaY;
 	}
-	currentX += deltaX;
+	currentY += deltaY;
 }
 
 void scroll_m_dl_A4_Maze_C_mesh_layer_4_vtx_1() {
 	int i = 0;
-	int count = 64;
-	int width = 32 * 0x20;
+	int count = 96;
+	int height = 32 * 0x20;
 
-	static int currentX = 0;
-	int deltaX;
+	static int currentY = 0;
+	int deltaY;
 	Vtx *vertices = segmented_to_virtual(m_dl_A4_Maze_C_mesh_layer_4_vtx_1);
 
-	deltaX = (int)(0.5 * 0x20) % width;
+	deltaY = (int)(-2.0 * 0x20) % height;
 
-	if (absi(currentX) > width) {
-		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
 	}
 
 	for (i = 0; i < count; i++) {
-		vertices[i].n.tc[0] += deltaX;
+		vertices[i].n.tc[1] += deltaY;
 	}
-	currentX += deltaX;
+	currentY += deltaY;
 }
 
 void scroll_m_dl_A4_Maze_D_mesh_layer_4_vtx_1() {
 	int i = 0;
-	int count = 32;
-	int width = 32 * 0x20;
+	int count = 64;
+	int height = 32 * 0x20;
 
-	static int currentX = 0;
-	int deltaX;
+	static int currentY = 0;
+	int deltaY;
 	Vtx *vertices = segmented_to_virtual(m_dl_A4_Maze_D_mesh_layer_4_vtx_1);
 
-	deltaX = (int)(0.5 * 0x20) % width;
+	deltaY = (int)(-2.0 * 0x20) % height;
 
-	if (absi(currentX) > width) {
-		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
 	}
 
 	for (i = 0; i < count; i++) {
-		vertices[i].n.tc[0] += deltaX;
+		vertices[i].n.tc[1] += deltaY;
 	}
-	currentX += deltaX;
+	currentY += deltaY;
 }
 
 void scroll_m_dl_A4_Maze_E_mesh_layer_4_vtx_1() {
 	int i = 0;
 	int count = 64;
-	int width = 32 * 0x20;
+	int height = 32 * 0x20;
 
-	static int currentX = 0;
-	int deltaX;
+	static int currentY = 0;
+	int deltaY;
 	Vtx *vertices = segmented_to_virtual(m_dl_A4_Maze_E_mesh_layer_4_vtx_1);
 
-	deltaX = (int)(0.5 * 0x20) % width;
+	deltaY = (int)(-2.0 * 0x20) % height;
 
-	if (absi(currentX) > width) {
-		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
 	}
 
 	for (i = 0; i < count; i++) {
-		vertices[i].n.tc[0] += deltaX;
+		vertices[i].n.tc[1] += deltaY;
 	}
-	currentX += deltaX;
+	currentY += deltaY;
 }
 
 void scroll_m_dl_A4_Room_B_mesh_layer_4_vtx_1() {
 	int i = 0;
 	int count = 64;
-	int width = 32 * 0x20;
+	int height = 32 * 0x20;
 
-	static int currentX = 0;
-	int deltaX;
+	static int currentY = 0;
+	int deltaY;
 	Vtx *vertices = segmented_to_virtual(m_dl_A4_Room_B_mesh_layer_4_vtx_1);
 
-	deltaX = (int)(0.5 * 0x20) % width;
+	deltaY = (int)(-2.0 * 0x20) % height;
 
-	if (absi(currentX) > width) {
-		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
 	}
 
 	for (i = 0; i < count; i++) {
-		vertices[i].n.tc[0] += deltaX;
+		vertices[i].n.tc[1] += deltaY;
 	}
-	currentX += deltaX;
+	currentY += deltaY;
+}
+
+void scroll_m_dl_A4_Room_C_mesh_layer_4_vtx_3() {
+	int i = 0;
+	int count = 184;
+	int height = 32 * 0x20;
+
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(m_dl_A4_Room_C_mesh_layer_4_vtx_3);
+
+	deltaY = (int)(-2.0 * 0x20) % height;
+
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentY += deltaY;
 }
 
 void scroll_gfx_mat_m_dl_Water_A_layer5() {
@@ -145,12 +191,14 @@ void scroll_gfx_mat_m_dl_Water_B_layer5() {
 };
 
 void scroll_m() {
+	scroll_m_dl_hub_sign_mesh_layer_4_vtx_0();
 	scroll_m_dl_A4_Maze_A_mesh_layer_4_vtx_1();
 	scroll_m_dl_A4_Maze_B_mesh_layer_4_vtx_1();
 	scroll_m_dl_A4_Maze_C_mesh_layer_4_vtx_1();
 	scroll_m_dl_A4_Maze_D_mesh_layer_4_vtx_1();
 	scroll_m_dl_A4_Maze_E_mesh_layer_4_vtx_1();
 	scroll_m_dl_A4_Room_B_mesh_layer_4_vtx_1();
+	scroll_m_dl_A4_Room_C_mesh_layer_4_vtx_3();
 	scroll_gfx_mat_m_dl_Water_A_layer5();
 	scroll_gfx_mat_m_dl_Water_B_layer5();
 };

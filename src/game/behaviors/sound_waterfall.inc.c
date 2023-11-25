b7783e5bd7877c5937c104b578857a0f8f65dc39
Drahnokks <mathiasduval@hotmail.fr>
2023-11-25 20:23:22 +0100

    waterfall emitter for sound consistency

--- src/game/behaviors/sound_waterfall.inc.c
+++ src/game/behaviors/sound_waterfall.inc.c
@@ -1,18 +1,13 @@
 // sound_waterfall.inc.c
 
 void bhv_waterfall_sound_loop(void) {
-
-    print_text_fmt_int(160, 180, "BPAR %d", GET_BPARAM12(o->oBehParams));
-    char str2[50];
-    sprintf(str2, "DIST %f", dist_between_objects(o, gMarioObject));
-    print_text(160, 160, str2);
-
     // if no distance no specified ignore comportement
     if(GET_BPARAM12(o->oBehParams) != 0){
         //if specified and in range
         if(dist_between_objects(o, gMarioObject) < GET_BPARAM12(o->oBehParams)){
-            print_text_fmt_int(160, 140, "COND %d", dist_between_objects(o, gMarioObject) < GET_BPARAM12(o->oBehParams));
-            play_sound(SOUND_ENV_WATERFALL2, gCurrentObject->header.gfx.cameraToObject);
+            struct Object *emitter = cur_obj_nearest_object_with_behavior_and_bparam1(bhvWaterfallSoundLoop, 1);
+            if(emitter != NULL)
+                play_sound(SOUND_ENV_WATERFALL2, emitter->header.gfx.cameraToObject);
         }
     } else {
         cur_obj_play_sound_1(SOUND_ENV_WATERFALL2);

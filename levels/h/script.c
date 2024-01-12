8febcdc6c16382ec8eba2e281290fa1e05a2ed2d
RRovert <rovertentertainment@gmail.com>
2024-01-11 21:11:59 -0600

    geothermal

--- levels/h/script.c
+++ levels/h/script.c
@@ -103,7 +103,7 @@ const LevelScript level_h_entry[] = {
 		OBJECT(MODEL_NONE, 13808, 39, 10605, 0, 0, 0, (1 << 16), bhvCoinFormation),
 		TERRAIN(h_area_1_collision),
 		MACRO_OBJECTS(h_area_1_macro_objs),
-		STOP_MUSIC(0),
+		SET_BACKGROUND_MUSIC(0x00, SEQ_H_GEOTHERMAL),
 		TERRAIN_TYPE(TERRAIN_STONE),
 		/* Fast64 begin persistent block [area commands] */
 		/* Fast64 end persistent block [area commands] */

d1d0feebe266716db7453fc613ca7dd8412e125d
RRovert <rovertentertainment@gmail.com>
2024-03-04 20:00:46 -0600

    hint file

---
+++ src/game/hints.c
@@ -0,0 +1,44 @@
+
+#include "hints.h"
+
+    //                                             | Max Newline Marker
+
+hint course_1_hints[] = {
+    {"How did you manage to miss this star?\n\
+    It's literally right next to the hub pipe...\n\
+    You missed this one on purpose, didn't you?",
+    0},
+
+    {"Collect all the red coins in the main\n\
+    area near the hub pipe.",
+    0},
+
+    {"Cross the bronto burt bridges and climb\n\
+    he mountain to find the star.",
+    0},
+
+    {"Cut down all the heavy objects to activate\n\
+    the cannon. Enter the cannon to get launched\n\
+    to the star.",
+    (ABILITY_FLAG_CUTTER)},
+
+    {"In the jungle area of the cave, there is\n\
+    a star piece switch. Some star pieces are\n\
+    hidden in the air, so be sure to look up.",
+    0},
+
+    {"In the cave, there are some metal grilles.\n\
+    Take advantage of the Phasewalk jump to\n\
+    platform your way up to the star.",
+    (ABILITY_FLAG_PHASEWALK)},
+
+    {"In the jungle area of the cave, there are\n\
+    some colored arrows that point to buttons.\n\
+    Shoot every single button to get the star!",
+    (ABILITY_FLAG_SHOCK_ROCKET)},
+
+    {"Follow the rails to the end of the cave.\n\
+    You will need the Gadget Watch to open the gate.\n\
+    Throw anything & everything you got at the boss!",
+    (ABILITY_FLAG_GADGET_WATCH)},
+};

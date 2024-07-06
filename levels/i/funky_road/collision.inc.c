7206633f6a4c3d71b086eb73c12654013612822f
RRovert <rovertentertainment@gmail.com>
2024-07-06 11:39:24 -0500

    fix shadows in level i

--- levels/i/funky_road/collision.inc.c
+++ levels/i/funky_road/collision.inc.c
@@ -5,7 +5,7 @@ const Collision funky_road_collision[] = {
 	COL_VERTEX(621, 0, -100),
 	COL_VERTEX(-621, 0, -100),
 	COL_VERTEX(-621, 0, 100),
-	COL_TRI_INIT(SURFACE_DEFAULT, 2),
+	COL_TRI_INIT(SURFACE_FORCE_SHADOW, 2),
 	COL_TRI(0, 1, 2),
 	COL_TRI(0, 2, 3),
 	COL_TRI_STOP(),
--- levels/i/plum/geo.inc.c
+++ levels/i/plum/geo.inc.c
@@ -1,7 +1,7 @@
 #include "src/game/envfx_snow.h"
 
 const GeoLayout plum_geo[] = {
-	GEO_NODE_START(),
+	GEO_SHADOW(SHADOW_CIRCLE_4_VERTS, 0xC8, 100),
 	GEO_OPEN_NODE(),
 		GEO_DISPLAY_LIST(LAYER_TRANSPARENT, plum_prune_mesh_layer_5),
 		GEO_DISPLAY_LIST(LAYER_OPAQUE, plum_prune_mesh_layer_1),

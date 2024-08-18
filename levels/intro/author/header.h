294525bc7c2c48c6fe26b9e8d0e052c632e13919
RRovert <rovertentertainment@gmail.com>
2024-08-17 23:30:27 -0500

    title screen rovertronic update

--- levels/intro/author/header.h
+++ levels/intro/author/header.h
@@ -1,8 +1,9 @@
 extern u8 author_round_corner_ia8[];
-extern u8 author_author_logo_ia4[];
-extern Vtx author_Plane_mesh_vtx_0[36];
+extern u8 author_rovertronic_host_ia8[];
+extern Vtx author_Plane_mesh_vtx_cull[8];
+extern Vtx author_Plane_mesh_vtx_0[28];
 extern Gfx author_Plane_mesh_tri_0[];
-extern Vtx author_Plane_mesh_vtx_1[4];
+extern Vtx author_Plane_mesh_vtx_1[30];
 extern Gfx author_Plane_mesh_tri_1[];
 extern Gfx mat_author_bg[];
 extern Gfx mat_revert_author_bg[];

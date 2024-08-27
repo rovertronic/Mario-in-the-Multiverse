9bcb3e2e0f2cf88109fbbb7690515ce5d8ae8e24
RRovert <rovertentertainment@gmail.com>
2024-08-26 23:50:21 -0500

    add chinatown m64 seq to k

--- levels/k/script.c
+++ levels/k/script.c
@@ -93,7 +93,7 @@ const LevelScript level_k_entry[] = {
 		OBJECT(MODEL_NONE, -8138, -1399, 10291, 0, 0, 0, (4 << 16), bhvWarp),
 		TERRAIN(k_area_1_collision),
 		MACRO_OBJECTS(k_area_1_macro_objs),
-		STOP_MUSIC(0),
+		SET_BACKGROUND_MUSIC(0x00, SEQ_K_CHINATOWN),
 		TERRAIN_TYPE(TERRAIN_STONE),
 		/* Fast64 begin persistent block [area commands] */
 		/* Fast64 end persistent block [area commands] */
